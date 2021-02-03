8d65f4d69ccee2e641f57e59613deb23dcaa2e56
bwapes <benwaples@gmail.com>
2021-02-02 18:22:41 -0800

    congfigure .env

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as dotenv from 'dotenv';
 
-function App() {
+dotenv.config({ path: `${__dirname}/.env` });
+
+function App(): JSX.Element {
+  console.log(
+    process.env.REACT_APP_GUARDIAN_KEY
+      ? process.env.REACT_APP_GUARDIAN_KEY
+      : 'fail'
+  );
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>Bens World</h3>
       </header>
     </div>
   );
