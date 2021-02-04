36e3ef5ef6b07a8c65d1801f3fb3b21cb21b4e7d
bwapes <benwaples@gmail.com>
2021-02-03 18:04:18 -0800

    rebase

--- src/components/articles/Article.tsx
+++ src/components/articles/Article.tsx
@@ -1,22 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import notFound from '../../services/notFound';
 import { Article as ArticleType } from '../../types';
+import './article.scss';
 
 export default function Article({
   article,
 }: {
   article: ArticleType;
 }): JSX.Element {
+  const [src, setSrc] = useState<string>();
+
   const imageSrc = article?.fields?.thumbnail
     ? article.fields.thumbnail
     : notFound;
 
+  useEffect(() => setSrc(imageSrc), [imageSrc]);
+
   return (
-    <li>
-      <h1>
-        <a href={article.webUrl}>{article.webTitle}</a>
-      </h1>
-      <img src={imageSrc} alt={article.webTitle} />
+    <li className="article">
+      <h1>{article.webTitle}</h1>
+      <img src={src} alt={article.webTitle} onError={() => setSrc(notFound)} />
+      <a href={article.webUrl}>Go to article</a>
     </li>
   );
 }
