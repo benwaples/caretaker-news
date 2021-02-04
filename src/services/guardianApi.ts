c657d2e2e027a06b703c22aab1526495327adc31
bwapes <benwaples@gmail.com>
2021-02-03 18:12:42 -0800

    rebase

--- src/services/guardianApi.ts
+++ src/services/guardianApi.ts
@@ -5,10 +5,7 @@ export function getArticles(search: string, page: number): Promise<Response> {
     `https://content.guardianapis.com/search?show-fields=thumbnail&q=${search}&page=${page}&api-key=${process.env.REACT_APP_GUARDIAN_KEY}`
   )
     .then((res) => res.json())
-    .then((json) => json.response)
-    .catch(() => {
-      throw new Error(`failed to fetch articles about ${search}`);
-    });
+    .then((json) => json.response);
 }
 
 export default getArticles;
