b05680a00223b12e3e7cfc80418914fed1ac7348
bwapes <benwaples@gmail.com>
2021-02-03 18:03:33 -0800

    rebase

--- src/services/guardianApi.ts
+++ src/services/guardianApi.ts
@@ -2,7 +2,7 @@ import { Response } from '../types';
 
 export function getArticles(search: string, page: number): Promise<Response> {
   return fetch(
-    `http://content.guardianapis.com/search?q=${search}&page=${page}&page-size=10&api-key=${process.env.REACT_APP_GUARDIAN_KEY}`
+    `https://content.guardianapis.com/search?show-fields=thumbnail&q=${search}&page=${page}&api-key=${process.env.REACT_APP_GUARDIAN_KEY}`
   )
     .then((res) => res.json())
     .then((json) => json.response)
