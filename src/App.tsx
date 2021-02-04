e0a4b2dbf24d1d36dfe5339b1a9cf11e420c9b88
bwapes <benwaples@gmail.com>
2021-02-03 17:50:48 -0800

    rebase

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+dotenv.config();
+
+function App(): JSX.Element {
+  console.log(process.env.REACT_APP_GUARDIAN_KEY);
   return (
     <div className="App">
       <header className="App-header">
